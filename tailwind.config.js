9136a40e41081f7fd078a554591f44e11db00d5b
Farook <mohammedfarook085@gmail.com>
2023-12-02 18:37:30 +0530

    add new font style

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        lexenddeca: ["Lexend Deca"],
+        bigshouldersdisplay: ["Big Shoulders Display"],
+      },
+    },
   },
   plugins: [],
 };
